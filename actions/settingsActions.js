2e26863535ada9b4de5c5fb7e845821f4c03f2f3
Aleksandr <exte92@gmail.com>
2022-07-13 16:49:53 +0300

    up bot settings

--- actions/settingsActions.js
+++ actions/settingsActions.js
@@ -6,29 +6,28 @@ const BOTS = new Map();
 const updateSettings = async (user, settings) => {
     const {botOn, sendSelf} = settings;
 
-    if(botOn && !BOTS.has(user.userId)) {
-        const bot = new Bot({...user, settings});
-        await bot.start();
-        BOTS.set(user.userId, bot);
+    if (botOn) {
+        if(!BOTS.has(user.userId)) {
+            const bot = new Bot({...user, settings});
+            await bot.start();
+            BOTS.set(user.userId, bot);
+        }
+
+        if(sendSelf !== user.settings.sendSelf) {
+            const bot = BOTS.get(user.userId);
+            await bot.stop();
+            BOTS.delete(user.userId);
+
+            const newBot = new Bot({...user, settings});
+            await newBot.start();
+            BOTS.set(user.userId, newBot);
+        }
     } else {
-        const bot = BOTS.get(user.userId);
-        if(bot) {
+        if(BOTS.has(user.userId)) {
+            const bot = BOTS.get(user.userId);
             await bot.stop();
             BOTS.delete(user.userId);
         }
-    }
-
-    if(botOn && sendSelf !== user.settings.sendSelf) {
-        const bot = BOTS.get(user.userId);
-
-        if(bot) {
-            await bot.stop();
-            BOTS.delete(user.userId);
-        }
-
-        const newBot = new Bot({...user, settings});
-        await newBot.start();
-        BOTS.set(user.userId, newBot);
     }
 }
 
